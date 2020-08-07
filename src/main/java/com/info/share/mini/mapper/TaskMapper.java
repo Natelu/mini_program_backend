dcbe7868ae96d17ea5d60cb31e1e7bc0c2d427e5
luzx23 <luzx23@chinaunicom.cn>
2020-08-07 16:10:43 +0800

    add comments

--- src/main/java/com/info/share/mini/mapper/TaskMapper.java
+++ src/main/java/com/info/share/mini/mapper/TaskMapper.java
@@ -22,16 +22,20 @@ public interface TaskMapper {
     void createTask(String id, String name, String service_wechat, String service_wechat_code, String introduce,
                     float money, String taskOwner);
 
+    // 任务详情 by taskID
     @Select("select id, name, service_wechat, service_wechat_code, introduce, money, publish_time, task_owner from task where id=#{id}")
     Task getTaskDetail(String id);
 
-    @Select("select id, name, service_wechat, service_wechat_code, introduce, money, publish_time, task_owner from task where name=#{name}")
+    // 任务详情 by taskName
+   @Select("select id, name, service_wechat, service_wechat_code, introduce, money, publish_time, task_owner from task where name=#{name}")
     Task getTaskDetailByName(String name);
 
+    // 任务领取
     @Insert("insert into do_task (id, user_id, task_id, status, create_time) values(#{id}, #{userId}, #{taskId}," +
             " #{status}, now())")
     void createDoTask(String id, String userId, String taskId, String status);
 
+    // 更改用户任务状态
     @Update("update do_task set status = #{status} where user_id=#{userId} and task_id=#{taskId}")
     void updateUserTask(String userId, String taskId, String status);
 }
